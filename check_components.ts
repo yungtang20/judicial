b596c57559639b413234cbcb7f92fb2d334bda8b
yungtang <yungtang2011@gmail.com>
2026-09-01 16:26:03 +0800

    chore: remove obsolete maintenance scripts

--- check_components.ts
+++
@@ -1,9 +0,0 @@
-import { Project, SyntaxKind } from "ts-morph";
-const project = new Project();
-const sourceFile = project.addSourceFileAtPath("src/components/SmartAppealAssistant.tsx");
-
-const components = sourceFile.getFunctions().filter(f => f.getName()?.match(/^[A-Z]/));
-const arrowComponents = sourceFile.getVariableDeclarations().filter(v => v.getName().match(/^[A-Z]/) && v.getInitializerIfKind(SyntaxKind.ArrowFunction));
-
-console.log("Functions:", components.map(c => c.getName()));
-console.log("Arrow components:", arrowComponents.map(c => c.getName()));
